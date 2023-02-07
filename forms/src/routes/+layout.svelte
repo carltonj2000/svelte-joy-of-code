23f8364d278418ea4e0bf3ece9e9e8d3ff19056a
Carlton Joseph <carlton.joseph@gmail.com>
2023-02-06 18:40:57 -0800

    add zod and zfd from validation

--- forms/src/routes/+layout.svelte
+++ forms/src/routes/+layout.svelte
@@ -5,11 +5,11 @@
 <nav>
 	<ul>
 		<li><a href="/">Home</a></li>
-		<li><a href="/todos">To Do's</a></li>
+		<li><a href="/todos">To Do's Latest</a></li>
 		<li><a href="/todos1">To Do's Take #1</a></li>
 		<li><a href="/todos2">To Do's Take #2</a></li>
-		<li><a href="/login">Login</a></li>
-		<li><a href="/login1">Login</a></li>
+		<li><a href="/login">Login Latest</a></li>
+		<li><a href="/login1">Login Take #1</a></li>
 	</ul>
 </nav>
 
--- forms/src/routes/login/+page.server.ts
+++ forms/src/routes/login/+page.server.ts
@@ -1,31 +1,22 @@
 import { fail, redirect, type Actions } from '@sveltejs/kit';
+import { zfd } from 'zod-form-data';
 
 export const actions: Actions = {
   default: async ({ request }) => {
     const formData = await request.formData();
-    const user = String(formData.get('user'));
-    const password = String(formData.get('password'));
-    console.log({ user, password });
 
-    const errors: Record<string, unknown> = {};
+    const loginSchema = zfd.formData({
+      user: zfd.text(),
+      password: zfd.text()
+    });
 
-    if (!user || typeof user !== 'string') {
-      errors.user = 'required';
+    const result = loginSchema.safeParse(formData);
+    const data = Object.fromEntries(formData);
+    if (!result.success) {
+      const errors = result.error.flatten().fieldErrors;
+      return fail(400, { data, errors });
     }
-
-    if (!password || typeof password !== 'string') {
-      errors.password = 'required';
-    }
-    const data = {
-      data: Object.fromEntries(formData),
-      errors
-    };
-
-    if (Object.keys(errors).length > 0) {
-      return fail(400, data);
-    }
-
-    return data;
-    //throw redirect(303, '/todos');
+    console.log({ data });
+    return { data };
   }
 };
